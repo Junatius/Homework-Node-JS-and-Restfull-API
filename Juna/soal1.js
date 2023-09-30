225b3cc52b51e5130c8b86783b51cc72e345c50d
Junatius <cjuniar0@gmail.com>
2023-09-30 09:10:29 +0800

    feat: using module that made from calc.js

---
+++ Juna/soal1.js
@@ -0,0 +1,7 @@
+const calc = require('./calc');
+
+calc.squareAreaAndPerimeter(3);
+calc.rectangleAreaAndPerimeter(5, 7);
+
+calc.squareAreaAndPerimeter(123);
+calc.rectangleAreaAndPerimeter(55, 56);
