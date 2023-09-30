5a3b8bb7c4f0d57abf5b4013ba66f131f5c5a7d1
Junatius <cjuniar0@gmail.com>
2023-09-30 09:10:04 +0800

    feat: add module for soal1.js

---
+++ Juna/calc.js
@@ -0,0 +1,15 @@
+exports.squareAreaAndPerimeter = function(squareSide) {
+    const area = squareSide ** 2;
+    const perimeter = squareSide * 4;
+    console.log(
+        `A square with the length of ${squareSide} cm has an Area of ${area} cm² and a perimeter of ${perimeter} cm.`
+    )
+}
+
+exports.rectangleAreaAndPerimeter = function(rectangleLength, rectangleWidth) {
+    const area = rectangleWidth * rectangleLength;
+    const perimeter = (2*rectangleWidth) + (2*rectangleLength);
+    console.log(
+        `A rectangle with a length of ${rectangleLength} and a width of ${rectangleWidth} cm has an Area of ${area} cm² and a perimeter of ${perimeter} cm.\n`
+    )
+}
